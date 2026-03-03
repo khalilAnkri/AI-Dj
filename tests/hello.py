f7e06a6f5825b96a4652f4614a02ebaed9fe391e
cha-mchn <charlottemichon@student.uliege.be>
2026-03-03 22:48:39 +0100

    modified path from GCS

---
+++ tests/hello.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import redirect, url_for 
+
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return f'Hello {guest} as Guest'
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+@app.route('/')
+def hello_world():
+    # return 'Hello, World!'
+    return "Hello from Flask!"
+
+@app.route('/about')
+def about():
+    return 'This is the about page'
+
+@app.route('/hello/<username>')
+def hello(username):
+    return f'Hello, {username}!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
