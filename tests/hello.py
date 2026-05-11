583796e8a9cb2eec8d98569f858f322c6d881f1d
ANKRI Mohamed Khalil <135461006+khalilAnkri@users.noreply.github.com>
2026-05-11 19:03:19 +0200

    Revert "Merge pull request #10 from khalilAnkri/develop"
    This reverts commit a61e03db37214fbaea83cadeb485743f3d409b1c, reversing
    changes made to 92bbcc760be0b8bb57e0ae4a4db2825c2da5a154.

--- tests/hello.py
+++ tests/hello.py
@@ -1,41 +1,36 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
+from flask import redirect, url_for 
 
 app = Flask(__name__)
 
-
-@app.route("/admin")
+@app.route('/admin')
 def hello_admin():
-    return "Hello Admin"
+    return 'Hello Admin'
 
-
-@app.route("/guest/<guest>")
+@app.route('/guest/<guest>')
 def hello_guest(guest):
-    return f"Hello {guest} as Guest"
+    return f'Hello {guest} as Guest'
 
-
-@app.route("/user/<name>")
+@app.route('/user/<name>')
 def hello_user(name):
-    if name == "admin":
-        return redirect(url_for("hello_admin"))
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
     else:
-        return redirect(url_for("hello_guest", guest=name))
+        return redirect(url_for('hello_guest', guest=name))
 
-
-@app.route("/")
+@app.route('/')
 def hello_world():
     # return 'Hello, World!'
     return "Hello from Flask!"
 
-
-@app.route("/about")
+@app.route('/about')
 def about():
-    return "This is the about page"
+    return 'This is the about page'
 
-
-@app.route("/hello/<username>")
+@app.route('/hello/<username>')
 def hello(username):
-    return f"Hello, {username}!"
+    return f'Hello, {username}!'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
